fcdd13aaa82f358c43f91e55a1c451cd51d31aed
a2bendah <a2bendah@enib.fr>
2026-03-01 16:54:06 +0100

    add incremntal strategy to btc model

--- models/stg_btc.sql
+++ models/stg_btc.sql
@@ -1,3 +1,16 @@
+{{
+  config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = 'hash_key'
+    )
+}}
+
+
 select
     *
 from {{ source('BTC_SCHEMA', 'BTC_TABLE') }}
+
+{% if is_incremental() %}
+    where BLOCK_TIMESTAMP >=select(max(BLOCK_TIMESTAMP) from {{this}})
+{% endif %}
